39223dcc2c2464db52ac94ed821e051b43e1daa3
Sandy Lee <sl30724@gmail.com>
2022-11-15 15:11:06 -0700

    added footer and fixed scrolling bugs

--- vite-fwd-project2/src/main.ts
+++ vite-fwd-project2/src/main.ts
@@ -45,6 +45,7 @@ document.querySelector<HTMLDivElement>(".tour")!.innerHTML = `
       <img class="content-image scooter" src="images/pig-scooter.png" alt="pig scooter">
       <img class="content-image scooter" src="images/vendor-scooter.png" alt="vendor scooter">
       </div>
+    <p><span lang="zh-Hant">機車 (motorcycles/scooters)</span></p>
     <p>In the morning, you might be waken up by the roaring sounds of scooters.<br>
     <br>
     Because of the well-developed public transportation system, owning a car is unnecessary for many. Nevertheless, having a small and convenient scooter is popular.<br>
@@ -61,16 +62,16 @@ document.querySelector<HTMLDivElement>(".tour")!.innerHTML = `
       </div>
       <p>Before we start exploring Taipei, we need to take care of our stomach.<br>
       <br>
-      In Taiwan, breakfast is believed to be the most important meal of the day. With the fast-paced lifestyle of Taipei, people usually don’t have time to make breakfast at home, but you can always find a <span lang="zh-Hant">早餐店</span> (breakfast shop) around every corner.<br>
+      In Taiwan, breakfast is believed to be the most important meal of the day. With the fast-paced lifestyle of Taipei, people usually don’t have time to make breakfast at home, but you can always find a <span lang="zh-Hant">早餐店 (breakfast shop)</span> around every corner.<br>
       </p>
     </div>
   </div>
   <div class="content" id="fourth">
     <img class="map-closeup" src="/images/mrt-station.png" alt="MRT station">
     <div class="copy">
-      <p><span lang="zh-Hant">捷運</span> (MRT, Taipei Mass Rapid Transit) is the most used public transportation system to get around Taipei with 131 stations and over two million trips made daily.<br>
+      <p><span lang="zh-Hant">捷運 (MRT, Taipei Mass Rapid Transit)</span> is the most used public transportation system to get around Taipei with 131 stations and over two million trips made daily.<br>
       <br>
-      You can hear the three most commonly-used languages in Taiwan from the announcements on MRT:  <span lang="zh-Hant">台語</span> (Taiwanese Hokkien), <span lang="zh-Hant">客家話</span> (Hakka), and <span lang="zh-Hant">中文/台灣華語</span> (Taiwanese Mandarin).<br>
+      You can hear the three most commonly-used languages in Taiwan from the announcements on MRT:  <span lang="zh-Hant">台語 (Taiwanese Hokkien)</span>, <span lang="zh-Hant">客家話 (Hakka)</span>, and <span lang="zh-Hant">中文/台灣華語 (Taiwanese Mandarin)</span>.<br>
       <br>
       Let’s take the MRT to get to our first destination!</p>
     </div>
@@ -85,7 +86,7 @@ document.querySelector<HTMLDivElement>(".tour")!.innerHTML = `
   <div class="content" id="sixth">
     <div class="copy">
       <p>While strolling through Taipei,<br>
-      you might hear the singing of <span lang="zh-Hant">垃圾車</span>(garbage truck).<br>
+      you might hear the singing of <span lang="zh-Hant">垃圾車 (garbage truck)</span>.<br>
       <br>
       The residents bring out the trash when they hear the familiar Beethoven's “Für Elise,” being played by the drivers of garbage trucks as they drive down the street. This is often a chance for neighbors to mingle, keeping the streets bustling with social interactions.</p>
     </div>
@@ -105,7 +106,85 @@ document.querySelector<HTMLDivElement>(".tour")!.innerHTML = `
     </div>
     <img class="map-closeup" src="/images/taipei-101.png" alt="garbage truck">
   </div>
+  <div class="content" id="eighth">
+  <img class="map-closeup" src="/images/lungshan-temple.png" alt="Lungshan Temple">
+    <div class="copy">
+      <p>Beneath Taipei’s modern façade is a body of beliefs that stretch back for millennia. That’s why temples are everywhere to be seen around Taipei.<br>
+      <br>
+      Head over to one of the most popular temples in Taipei, <span lang="zh-Hant">龍山寺 (Lungshan Temple)</span>, and learn how religion sets the clock for living in Taiwan, regulates holidays, and often determines the rhythm for both business and family.</p>
+    </div>
+  </div>
+  <div class="content" id="nineth">
+  <img class="map-closeup" src="/images/dadaocheng.png" alt="Dadaocheng">
+    <div class="copy">
+      <p>Head to the nearby <span lang="zh-Hant">大稻埕 (Dadaocheng, Twatutia, Daitōtei)</span>, the oldest district in Taipei, for a glimpse at the old Taipei where most of the buildings still preserve its original looks.<br>
+      <br>
+      You can also watch a  <span lang="zh-Hant">歌仔戲 (Taiwanese opera, Ke-Tse opera, Hokkien opera)</span> at Dadaocheng Theater, a form of traditional drama originating in Taiwan. It is considered as the embodiment of Taiwanese history and tradition because the political identity crisis of Taiwan shaped its development.<br>
+      <br>
+      While its popularity among people has declined over the years, there’s a dedicating group of people striving to preserve this spectacular art form.</p>
+    </div>
+  </div>
+  <div class="content" id="tenth">
+  <img class="map-closeup" id="map-nightmarket" src="/images/background-night-market.png" alt="Night market">
+    <div class="copy">
+      <div class="gallery">
+        <div class="image-caption">
+          <img class="content-image nightmarket" src="images/stinky-tofu.png" alt="stinky tofu">
+          <p class=caption><span lang="zh-Hant">臭豆腐<br>(Stinky tofu)</span></p>
+        </div>
+        <div class="image-caption">
+          <img class="content-image nightmarket" src="images/bubble-tea.png" alt="bubble tea">
+          <p class=caption><span lang="zh-Hant">珍珠奶茶<br>(Bubble tea)</span></p>
+        </div>
+        <div class="image-caption">
+          <img class="content-image nightmarket" src="images/oyster-omlette.png" alt="oyster omlette">
+          <p class=caption><span lang="zh-Hant">蚵仔煎<br>(Oyster Omlette)</span></p>
+        </div>
+        <div class="image-caption">
+          <img class="content-image nightmarket" src="images/meatball.png" alt="meatball">
+          <p class=caption><span lang="zh-Hant">肉圓<br>(Meatball)</span></p>
+        </div>
+        <div class="image-caption">
+          <img class="content-image nightmarket" src="images/papaya-milk.png" alt="papaya milk">
+          <p class=caption><span lang="zh-Hant">木瓜牛奶<br>(Papaya milk)</span></p>
+        </div>
+        <div class="image-caption">
+          <img class="content-image nightmarket" src="images/mango-ice.png" alt="mango ice">
+          <p class=caption><span lang="zh-Hant">芒果冰<br>(Mango ice)</span></p>
+        </div>
+        <div class="image-caption">
+          <img class="content-image nightmarket" src="images/pigblood-cake.png" alt="pigblood cake">
+          <p class=caption><span lang="zh-Hant">豬血糕<br>(Pigbloog cake)</span></p>
+        </div>
+      </div>
+      <p>When the nightfall approaches, it’s time to head over the highlight of your day — <span lang="zh-Hant">夜市 (night market)</span>. All kinds of vendors would gather around urban street corners or temple plazas to sell handicrafts, games, consumer goods,  Taiwanese street food, and desserts.<br>
+      </p>
+    </div>
+  </div>
  </div>
+`
+document.querySelector<HTMLDivElement>(".footer")!.innerHTML = `
+<div class="intro-block">
+    <h1><span lang="zh-Hant">謝謝！</span></h1>
+    <p>This project wouldn’t be as cool if not for the beautiful illustrations handdrawn by Tom Parker,<br>
+    as well as the multiple sound clips poduced by people who share my love for Taipei City.<br>
+    See below for the the list of Youtube videos used for audio,<br>
+    or see the tour again by clicking the button.
+    </p>
+    <button class="primary" id= "restart-button" type="button">Start Over</button>
+    <p class=caption><a target="_blank" href="https://youtu.be/yIlEh7P4o04">China Airlines A350-900 landing @ Taoyuan [Shot on α6300]</a><br>
+    <a target="_blank" href="https://youtu.be/j6dp21iXdhI">4K Taipei Ximending Videowalk At Night | 3D Street Binaural Sound iPhone 11</a><br>
+    <a target="_blank" href="https://youtu.be/0I7H8VQ6RpA">Scooter Waterfall In Taipei</a><br>
+    <a target="_blank" href="https://youtu.be/Id1KfxiKdeY">Taiwan style breakfast @ TaiChung - Taiwan Street Food</a><br>
+    <a target="_blank" href="https://youtu.be/i2vK6-dteH0">Taiwan, Taipei, 2X MRT ride from Main station, to Taipei Zoo</a><br>
+    <a target="_blank" href="https://on.soundcloud.com/Ly5Uk">Audio Postcard Yinhe Cave (銀河洞) Hiking Trail, New Taipei City, Xindian District, Taiwan, 17112019</a><br>
+    <a target="_blank" href="https://youtu.be/DRvgmLLlhp0">Musical Trash Truck in Taiwan</a><br>
+    <a target="_blank" href="https://youtu.be/0bakTGk3Arg">[4K] Walking in Taipei - Night - Taipei 101 Food Court - (Dji Osmo Pocket 2)</a><br>
+    <a target="_blank" href="https://youtu.be/DRvgmLLlhp0">Longshan Temple - 龍山寺 - Taipei, Taiwan - 4K</a><br>
+    <a target="_blank" href="https://youtu.be/EcGZbyVXZu0">宜蘭~蘭陽歌劇團~大稻埕戲苑~好戲演出~錯配姻緣</a><br>
+    <a target="_blank" href="https://youtu.be/YIchktTfiD0">Popular Taiwanese Street and Night Market Food | Taiwanese Street Food</a><br>
+    </p>
+    </div>
 `
 
 // apparently with querySelector you can use the following syntax for dom
--- vite-fwd-project2/src/scroll.ts
+++ vite-fwd-project2/src/scroll.ts
@@ -4,19 +4,19 @@ import { ScrollToPlugin, ScrollTrigger } from "gsap/all";
 import { Howl, Howler } from "howler";
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-// const pages = Array.from(document.querySelectorAll<HTMLElement>(".page"));
+const pages = Array.from(document.querySelectorAll<HTMLElement>(".page"));
 const intro = document.querySelector<HTMLElement>(".tour")!;
 const startButton = document.getElementById("start-button") as HTMLButtonElement;
+const restartButton = document.getElementById("restart-button") as HTMLButtonElement;
 const soundButton = document.getElementById("sound-button") as HTMLButtonElement;
 const warning = document.querySelector<HTMLElement>(".warning")!;
-// const background = document.querySelector<HTMLElement>(".background")!;
-// const mrt = document.getElementById("mrt") as HTMLImageElement;
+const background = document.querySelector<HTMLElement>(".background")!;
 let muteBool: boolean = false;
 // Howler setup
 Howler.volume(0.8);
 
-let mySrc: Array<string> = ["/audios/238kb-edited-plane-landing-china-.mp3", "/audios/750kb-edited-ximending-walking.mp3", "/audios/356kb-edited-motorcycle-waterfall.mp3", "/audios/519kb-edited-breakfast.mp3", "/audios/1.7mb-edited-mrt.mp3", "/audios/553kb-edited-hiking.mp3", "/audios/430kb-edited-garbage-truck.mp3", "/audios/680kb-edited-food-court.mp3"];
-// "/audios/1.7mb-edited-mrt.mp3", "/audios/553kb-edited-hiking.mp3", "/audios/430kb-edited-garbage-truck.mp3", "/audios/680kb-edited-food-court.mp3"
+let mySrc: Array<string> = ["/audios/238kb-edited-plane-landing-china-.mp3", "/audios/750kb-edited-ximending-walking.mp3", "/audios/356kb-edited-motorcycle-waterfall.mp3", "/audios/519kb-edited-breakfast.mp3", "/audios/1.7mb-edited-mrt.mp3", "/audios/553kb-edited-hiking.mp3", "/audios/430kb-edited-garbage-truck.mp3", "/audios/680kb-edited-food-court.mp3", "/audios/352kb-edited-longshan-temple.mp3", "/audios/815kb-edited-taiwanese-opera.mp3", "audios/426kb-edited-night-market.mp3"];
+
 let sounds = new Array(mySrc.length);
 mySrc.forEach((src, i) => {
     sounds[i] = new Howl({
@@ -25,7 +25,7 @@ mySrc.forEach((src, i) => {
         loop: true,
         onplay: function () {
             console.log(`src ${i} is playing`);
-            sounds[i].fade(0, 0.8, 3000);
+            sounds[i].fade(0, 0.8, 2000);
         },
         onstop: function () {
             // sounds[i].fade(0.8, 0, 3000);
@@ -35,27 +35,31 @@ mySrc.forEach((src, i) => {
 });
 
 // scroll with background pinned
-ScrollTrigger.create({
-    trigger: "section.tour",
-    pin: ".background",
-    start: "top top",
-    endTrigger: ".content-block",
-    immediateRender: false,
-    scrub: 0.5,
-    // snap: 1/5,
-    end: () => "bottom top-=" + window.innerHeight,
-});
+
 
 // DOM & GSAP & Howl
 startButton.addEventListener("click", () => {
-    console.log("button pressed");
-    // pages.forEach(page => {
-    if (intro.style.display !== "flex") {
-        intro.style.display = "flex";
+    pages.forEach(page => {
+    if (page.style.display !== "flex") {
+        page.style.display = "flex";
     };
-    // });
+    });
     gsap.to(window, { duration: 1, scrollTo: { y: ".tour" }, immediateRender: false });
+    ScrollTrigger.create({
+        trigger: "section.tour",
+        pin: ".background",
+        start: "top top",
+        end: "bottom bottom",
+        endTrigger: ".content-block",
+        immediateRender: false,
+        scrub: 0.5,
+        // end: () => "bottom top-=" + window.innerHeight/2,
+    });
     sounds[0].play();
+});
+
+restartButton.addEventListener("click", () => {
+    gsap.to(window, { duration: 1, scrollTo: { y: ".tour" }, immediateRender: false });
 });
 
 soundButton.addEventListener("click", () => {
@@ -65,7 +69,7 @@ soundButton.addEventListener("click", () => {
 });
 
 //play sounds when an element is in viewport
-let contents: Array<string> = ["start", "first", "second", "third", "fourth", "fifth", "sixth", "seventh"];
+let contents: Array<string> = ["start", "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "nineth", "tenth"];
 
 for (let i = 0; i < contents.length; i++) {
     ScrollTrigger.create({
@@ -93,7 +97,7 @@ for (let i = 0; i < contents.length; i++) {
                 }
             }
             else if (self.isActive === false) {
-                sounds[i].fade(0.8, 0, 2000);
+                sounds[i].fade(0.8, 0, 1000);
                 if (i === 0) {
                     sounds[i].stop();
                 };
@@ -114,4 +118,33 @@ function checkMute() {
         soundButton.style.color = "black";
         warning.style.display = "none";
     }
+};
+
+// Set the name of the hidden property and the change event for visibility
+let hidden: string;
+let visibilityChange;
+if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
+};
+
+// If the page is hidden, stop the audio;
+function handleVisibilityChange() {
+  if (document[hidden]) {
+    Howler.stop();
+  } 
+};
+
+// Warn if the browser doesn't support addEventListener or the Page Visibility API
+if (typeof document.addEventListener === "undefined" || hidden === undefined) {
+  console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
+} else {
+  // Handle page visibility change
+  document.addEventListener(visibilityChange, handleVisibilityChange, false);
 };
